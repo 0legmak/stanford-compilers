1f287ccf41ae8f5652b61eef0a990a7306f1ec20
olegmak <oleg.makovski@gmail.com>
2025-06-09 22:40:27 +0300

    Class inheritance check

--- assignments/PA4/semant.cc
+++ assignments/PA4/semant.cc
@@ -6,6 +6,11 @@
 #include "semant.h"
 #include "utilities.h"
 
+#include <optional>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
 
 extern int semant_debug;
 extern char *curr_filename;
@@ -81,12 +86,125 @@ static void initialize_constants(void)
     val         = idtable.add_string("_val");
 }
 
+Symbol class__class::get_name() {
+    return name;
+}
 
+Symbol class__class::get_parent() {
+    return parent;
+}
+
+
+const std::string kObjectClassName = "Object";
+
+bool is_non_inheritable_class(const std::string& name) {
+    return name == "Int" || name == "Bool" || name == "String";
+}
+
+// This is a structure that may be used to contain the semantic
+// information such as the inheritance graph.  You may use it or not as
+// you like: it is only here to provide a container for the supplied
+// methods.
+
+class ClassTable {
+public:
+    ClassTable(Classes);
+    int errors() { return semant_errors; }
+    ostream& semant_error();
+    ostream& semant_error(Class_ c);
+    ostream& semant_error(Symbol filename, tree_node *t);
+
+private:
+    void install_basic_classes();
+    void add_class(const Class_ cls) {
+        const std::string name = cls->get_name()->get_string();
+        if (!class_map.insert({ name, cls }).second) {
+            semant_error(cls) << "Class " << name << " is already defined." << std::endl;
+        }
+    }
+    
+    int semant_errors;
+    ostream& error_stream;
+    std::unordered_map<std::string, Class_> class_map;
+};
 
 ClassTable::ClassTable(Classes classes) : semant_errors(0) , error_stream(cerr) {
-
-    /* Fill this in */
-
+    install_basic_classes();
+    for (int i = classes->first(); classes->more(i); i = classes->next(i)) {
+        add_class(classes->nth(i));
+    }
+    if (class_map.find("Main") == class_map.end()) {
+        semant_error() << "Main class not found in class list." << std::endl;
+    }
+    const int class_count = class_map.size();
+    std::unordered_map<std::string, int> class_name_to_id;
+    std::vector<std::string> class_id_to_name(class_count);
+    int class_name_id = 0;
+    for (const auto& [name, cls] : class_map) {
+        class_name_to_id[name] = class_name_id;
+        class_id_to_name[class_name_id] = name;
+        ++class_name_id;
+    }
+    std::vector<std::vector<int>> inheritance_graph(class_count);
+    for (const auto& [name, cls] : class_map) {
+        if (name == kObjectClassName) {
+            continue;
+        }
+        std::string parent_name = cls->get_parent()->get_string();
+        if (parent_name.empty()) {
+            parent_name = kObjectClassName;
+        }
+        if (const auto parent_class_iter = class_map.find(parent_name); parent_class_iter == class_map.end()) {
+            semant_error(cls) << "Parent class " << parent_name << " is not defined." << std::endl;
+            continue;
+        }
+        if (is_non_inheritable_class(parent_name)) {
+            semant_error(cls) << "Class " << parent_name << " is non-inheritable and cannot be a parent class." << std::endl;
+            continue;
+        }
+        inheritance_graph[class_name_to_id[parent_name]].push_back(class_name_to_id[name]);
+    }
+    std::vector<bool> visited(class_count);
+    std::vector<bool> on_stack(class_count);
+    std::vector<int> cycle;
+    std::optional<int> cycle_start;
+    auto cycle_check = [&](auto&& cycle_check, int u) -> bool {
+        if (on_stack[u]) {
+            cycle_start = u;
+            return false;
+        }
+        if (visited[u]) {
+            return true;
+        }
+        visited[u] = true;
+        on_stack[u] = true;
+        for (const int v : inheritance_graph[u]) {
+            if (!cycle_check(cycle_check, v)) {
+                break;
+            }
+        }
+        on_stack[u] = false;
+        if (cycle_start) {
+            cycle.push_back(u);
+            if (u == cycle_start) {
+                cycle_start.reset();
+            }
+        }
+        return cycle.empty();
+    };
+    for (int u = 0; u < class_count; ++u) {
+        if (!visited[u]) {
+            if (!cycle_check(cycle_check, u)) {
+                auto& err_stream = semant_error(class_map[class_id_to_name[cycle.front()]]);
+                err_stream << "Inheritance graph contains a cycle: ";
+                for (const int u : cycle) {
+                    err_stream << class_id_to_name[u] << " inherits ";
+                }
+                err_stream << class_id_to_name[cycle.front()] << std::endl;
+                break;
+            }
+        }
+    }
 }
 
 void ClassTable::install_basic_classes() {
@@ -122,6 +240,7 @@ void ClassTable::install_basic_classes() {
 					       single_Features(method(type_name, nil_Formals(), Str, no_expr()))),
 			       single_Features(method(copy, nil_Formals(), SELF_TYPE, no_expr()))),
 	       filename);
+    class_map[Object->get_string()] = Object_class;
 
     // 
     // The IO class inherits from Object. Its methods are
@@ -143,6 +262,7 @@ void ClassTable::install_basic_classes() {
 					       single_Features(method(in_string, nil_Formals(), Str, no_expr()))),
 			       single_Features(method(in_int, nil_Formals(), Int, no_expr()))),
 	       filename);  
+    class_map[IO->get_string()] = IO_class;
 
     //
     // The Int class has no methods and only a single attribute, the
@@ -153,12 +273,14 @@ void ClassTable::install_basic_classes() {
 	       Object,
 	       single_Features(attr(val, prim_slot, no_expr())),
 	       filename);
+    class_map[Int->get_string()] = Int_class;
 
     //
     // Bool also has only the "val" slot.
     //
     Class_ Bool_class =
 	class_(Bool, Object, single_Features(attr(val, prim_slot, no_expr())),filename);
+    class_map[Bool->get_string()] = Bool_class;
 
     //
     // The class Str has a number of slots and operations:
@@ -188,6 +310,7 @@ void ClassTable::install_basic_classes() {
 						      Str, 
 						      no_expr()))),
 	       filename);
+    class_map[Str->get_string()] = Str_class;
 }
 
 ////////////////////////////////////////////////////////////////////
